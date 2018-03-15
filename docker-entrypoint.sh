086adca9e9eb6be5d9d9ee66d20739e87118a450
Francis Lavalliere <francis.lavalliere@acentera.com>
2018-03-15 08:51:39 -0500

    added submodules
    added sample hugosite
    
    updated to ensure local node_modules
    
    added ignore of node_modules
    
    updated atom to include initial folders
    
    initial data
    
    updated to add all projects
    
    added atom to git ignore file
    
    added entry point to launch programs..
    
    added atom and fixed entrypooint commands

---
+++ docker-entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd $HOME;
+ln -snf /data/node_modules
+cd /data;
+for d in $(find . -mindepth 1 -maxdepth 1 -type d | grep -v \.git | grep -v node_modules); do
+   cd /data/$d;
+   ln -snf /data/node_modules
+   if [ -e package.json ]; then
+      echo "Will run npm install from $(pwd)"
+      npm install
+   fi;
+done;
+cd /data/grapejs-web-editor;
+npm start dev & 
+cd /data/serverless-cms-admin;
+npm run dev &
+cd /data/serverless-sample-hugosite;
+if [ -e /data/hugo ]; then
+   ../hugo serve --bind 0.0.0.0 --forceSyncStatic --destination public --enableApi=true --enableDev=true &
+else
+   hugo serve --bind 0.0.0.0 --forceSyncStatic --destination public --enableApi=true &
+fi;
+/usr/bin/atom -f
